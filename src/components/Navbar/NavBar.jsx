624b3663d3388f0e0d7d8581bf7bfb61b4f7a0ff
Alal Hosen <alalhosen1992620120@gmail.com>
2025-10-04 09:51:17 +0600

    fake data create  & set

---
+++ src/components/Navbar/NavBar.jsx
@@ -0,0 +1,19 @@
+
+const NavBar = () => {
+
+    const routes = [
+  { id: 1, path: "/", name: "Home" },
+  { id: 2, path: "/about", name: "About" },
+  { id: 3, path: "/services", name: "Services" },
+  { id: 4, path: "/contact", name: "Contact" },
+  { id: 5, path: "/profile", name: "Profile" }
+];
+
+    return (
+        <nav>
+            
+        </nav>
+    );
+};
+
+export default NavBar;
