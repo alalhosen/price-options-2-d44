33682b9f0086ba1561f3128636b4c173d9f3bab3
Alal Hosen <alalhosen1992620120@gmail.com>
2025-10-06 23:17:45 +0600

    set responsive icon in navbar

--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
 import Link from "../Link/Link";
-import { IoMenu,} from "react-icons/io5";
+import { IoMenu } from "react-icons/io5";
 import { IoMdClose } from "react-icons/io";
 
 const NavBar = () => {
-  const [open, setOpen]=useState(false)
+  const [open, setOpen] = useState(false);
   const routes = [
     { id: 1, path: "/", name: "Home" },
     { id: 2, path: "/about", name: "About" },
@@ -14,16 +14,20 @@ const NavBar = () => {
   ];
 
   return (
-    <nav>
-      <div className="md:hidden" onClick={()=>setOpen(!open)}>
-        {
-          open===true?
-          < IoMdClose></IoMdClose> 
-          :<IoMenu className="text-2xl"></IoMenu>
-        }
-        
+    <nav className="text-black bg-yellow-200 p-6">
+      <div className="md:hidden" onClick={() => setOpen(!open)}>
+        {open === true ? (
+          <IoMdClose></IoMdClose>
+        ) : (
+          <IoMenu className="text-2xl"></IoMenu>
+        )}
       </div>
-      <ul className="md:flex">
+
+      <ul
+        className={`md:flex duration-2000 absolute 
+        ${open ? 'top-12' : '-top-60'}
+        bg-yellow-500 px-6 shadow-lg`}
+      >
         {routes.map((route) => (
           <Link key={route.id} route={route}></Link>
         ))}
