8d4c964c355b561bf9841b4d2bec4917d59c5418
Alal Hosen <alalhosen1992620120@gmail.com>
2025-10-07 00:10:10 +0600

    create a new component priceOption

--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -17,16 +17,16 @@ const NavBar = () => {
     <nav className="text-black bg-yellow-200 p-6">
       <div className="md:hidden" onClick={() => setOpen(!open)}>
         {open === true ? (
-          <IoMdClose></IoMdClose>
+          <IoMdClose className="text-2xl"></IoMdClose>
         ) : (
           <IoMenu className="text-2xl"></IoMenu>
         )}
       </div>
 
       <ul
-        className={`md:flex duration-2000 absolute 
-        ${open ? 'top-12' : '-top-60'}
-        bg-yellow-500 px-6 shadow-lg`}
+        className={`md:flex duration-1000 absolute md:static 
+        ${open ? 'top-14' : '-top-60'}
+        bg-yellow-200 px-6`}
       >
         {routes.map((route) => (
           <Link key={route.id} route={route}></Link>
