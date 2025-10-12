45266cb4644eb0bf7a0409e6f81e7578accb5f71
Alal Hosen <alalhosen1992620120@gmail.com>
2025-10-12 22:20:01 +0600

    set axios

--- src/components/Phones/Phones.jsx
+++ src/components/Phones/Phones.jsx
@@ -1,12 +1,20 @@
+import axios from "axios";
 import { useEffect, useState } from "react";
 
 const Phones = () => {
   const [phones, setPhones] = useState([]);
-  useEffect(()=>{
-    fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
-  }, [])
+  useEffect(() => {
+    // fetch("https://openapi.programming-hero.com/api/phones?search=iphone")
+    //   .then((res) => res.json())
+    //   .then((data) => setPhones(data.data));
 
-  return <div></div>;
+    axios.get('https://openapi.programming-hero.com/api/phones?search=iphone')
+    .then(data=>console.log(data.data.data))
+  }, []);
+
+  return <div>
+    <h2 className="text-5xl">Phones: {phones.length}</h2>
+  </div>;
 };
 
 export default Phones;
