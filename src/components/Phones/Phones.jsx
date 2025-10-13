ff5f3ffe4e90d9c546f74e12d4521fe64a306bb3
Alal Hosen <alalhosen1992620120@gmail.com>
2025-10-13 23:48:08 +0600

    inport barChart

--- src/components/Phones/Phones.jsx
+++ src/components/Phones/Phones.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { BarChart, Bar, ResponsiveContainer, XAxis } from "recharts";
 
 const Phones = () => {
   const [phones, setPhones] = useState([]);
@@ -13,20 +14,24 @@ const Phones = () => {
       .then((data) => {
         const phoneData = data.data.data;
         const phoneWithFakeData = phoneData.map((phone) => {
-            const obj={
-                name:phone.phone_name,
-                price:parseInt(phone.slug.split('_')[3])
-            }
-            return obj;
-        })
+          const obj = {
+            name: phone.phone_name,
+            price: parseInt(phone.slug.split("_")[3]),
+          };
+          return obj;
+        });
         console.log(phoneWithFakeData);
-        setPhones(phoneWithFakeData)
+        setPhones(phoneWithFakeData);
       });
   }, []);
 
   return (
     <div>
       <h2 className="text-5xl">Phones: {phones.length}</h2>
+      <BarChart width={600} height={600} data={phones}>
+        <Bar dataKey="price" fill="#8884d8" />
+        <XAxis dataKey="name"></XAxis>
+      </BarChart>
     </div>
   );
 };
